04d32d2ad5a9081cfe2a4a83e27f79e417bdd065
zy_win <281265244@qq.com>
2022-05-02 18:12:37 +0800

    modify utils.py

--- utils.py
+++ utils.py
@@ -9,8 +9,9 @@ import time
 import math
 
 import torch.nn as nn
-import torch.nn.init as init
 
+import torch.nn.init as init
+print(11)
 
 def get_mean_and_std(dataset):
     '''Compute the mean and std value of dataset.'''
